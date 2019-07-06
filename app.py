897d5723b0edd9e52e5419fd13f29c32387394dc
Bhaskar Singh <bhaskarsingh161197@gmail.com>
2019-07-06 16:04:19 +0530

    Started the flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template,request,url_for,redirect,session,flash
+from db import *
+from functools import wraps
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+global temp
+temp = False
+conn = connectDB()
+cursor = conn.cursor(dictionary=True)
+
+@app.route("/")
+def menu():
+    return render_template("home.html")

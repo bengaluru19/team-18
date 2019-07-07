a96e1f00bf120034121eeca19bf22af301403100
Bhaskar Singh <bhaskarsingh161197@gmail.com>
2019-07-07 10:51:21 +0530

    This is the final version

--- vendor/app.py
+++ vendor/app.py
@@ -60,5 +60,33 @@ def reject():
 
 @app.route('/current')
 def current():
-    qs = 
-    return render_template('current.html')
+    qs = "SELECT o.orderId,c.name, o.items, o.quantity, o.modeOfPayment, o.modeOfPayment FROM orders o, customers c where o.customerId=c.customerId AND o.vendorId=1 AND o.status='accept'"
+    cursor.execute(qs)
+    res = cursor.fetchall()
+    fin = res
+    totalitem = []
+    ls = []
+    totalquant = []
+    for i in res:
+        items = i["items"].split(':')
+        # print(items)
+        items = list(map(int, items))
+        
+        quantity = i["quantity"].split(':')
+        quantity = list(map(int, quantity))
+        its = []
+        for i in items:
+            qs = "SELECT * FROM item where itemid="+str(i)
+            cursor.execute(qs)
+            res = cursor.fetchone()
+            its.append(res["itemName"])
+        # print(its, quantity)
+        totalitem.append(its)
+        ls.append(len(its))
+        totalquant.append(quantity)
+    print(fin, totalitem, totalquant, len(fin), ls)
+    return render_template('current.html', rec=fin, items=totalitem, quantity=totalquant, l=len(fin), ll=ls)
+
+@app.route('/close')
+def close():
+    return render_template("invoice.html")
